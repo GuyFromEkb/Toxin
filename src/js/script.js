707c72a6f263b72954ac5fbcfd31bbeef2e3be90
Sergey <graygmai1@gmail.com>
2022-06-05 04:17:54 +0500

    update nav menu

--- src/js/script.js
+++ src/js/script.js
@@ -2,13 +2,18 @@
 window.addEventListener('DOMContentLoaded', () => {
 
     //nav Bold
-    const liItem = document.querySelectorAll('.nav__item');
-    liItem.forEach(item => {
-        item.style.width = (item.offsetWidth + 1.5) + "px";
-        console.log(item.offsetWidth);
-    });
+    // const liItem = document.querySelectorAll('.nav__item');
+    // liItem.forEach(item => {
+    //     item.style.width = (item.offsetWidth + 20) + "px";
+    // });
 
 
 
 
 });
+
+// const liItem = document.querySelectorAll('.nav__item');
+// liItem.forEach(item => {
+//     console.log(item.getBoundingClientRect().width);
+//     item.style.width = (item.clientWidth + 20) + "px";
+// });
